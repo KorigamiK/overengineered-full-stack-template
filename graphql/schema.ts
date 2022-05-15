702c81bf7091b2a529d789a7a7f0562a872b5cba
korigamik <korigamik@gmail.com>
2022-05-15 22:16:20 +0530

    add graphql

---
+++ graphql/schema.ts
@@ -0,0 +1,19 @@
+// graphql/schema.ts
+
+import { gql } from "apollo-server-micro";
+
+export const typeDefs = gql`
+  type Link {
+    id: String
+    title: String
+    description: String
+    url: String
+    category: String
+    imageUrl: String
+    users: [String]
+  }
+
+  type Query {
+    links: [Link]!
+  }
+`;
